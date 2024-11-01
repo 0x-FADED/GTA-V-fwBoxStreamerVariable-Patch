f6660339a3e9fa131a2bc87d763c2b8c167be093
_Sakura <97910292+0x-FADED@users.noreply.github.com>
2024-11-02 07:12:56 +0900

    sync changes

--- dllmain.cpp
+++ dllmain.cpp
@@ -25,18 +25,18 @@ struct DecalDef
 	char pad[80];
 };
 
-int kNumDecalDefsSize;
-int fwBoxStreamerVariableSize;
+int kNumDecalDefsSize = 0;
+int fwBoxStreamerVariableSize = 0;
 
-static void* decalDefStore;
+static void* decalDefStore = nullptr;
 
 
 void TomlShit()
 {
-	kNumDecalDefsSize = GetPrivateProfileInt("DecalLimitPatcher", "DecalDefs", -1, ".\\fwBoxStreamerVariable_DecalsLimit-Patch.toml");
+	kNumDecalDefsSize = GetPrivateProfileInt(L"DecalLimitPatcher", L"DecalDefs", -1, L".\\fwBoxStreamerVariable_DecalsLimit-Patch.toml");
 	kNumDecalDefsSize = kNumDecalDefsSize <= 0 ? 0x420 : kNumDecalDefsSize;
 
-	fwBoxStreamerVariableSize = GetPrivateProfileInt("fwBoxStreamerVariableSizePatcher", "fwBoxStreamerVariableSize", -1, ".\\fwBoxStreamerVariable_DecalsLimit-Patch.toml");
+	fwBoxStreamerVariableSize = GetPrivateProfileInt(L"fwBoxStreamerVariableSizePatcher", L"fwBoxStreamerVariableSize", -1, L".\\fwBoxStreamerVariable_DecalsLimit-Patch.toml");
 	fwBoxStreamerVariableSize = fwBoxStreamerVariableSize <= 0 ? 0x204B0 : fwBoxStreamerVariableSize;
 }
 
@@ -140,30 +140,12 @@ void fwBoxStreamerVariablePatch()
 
 	auto mnbvhList = hook::AllocateStubMemory(sizeof(void*) * fwBoxStreamerVariableSize);
 
-	/**
-	{
-		// rage::fwBoxStreamer::GetIntersectingAABB
-		auto address = hook::get_pattern<char>("48 83 EC 68 45 8A F8", 0x2E);
-		hook::put<int32_t>(address, (char*)mnbvhList - address + 4);
-		hook::put<int32_t>(address + 0x69, fwBoxStreamerVariableSize);
-	}
-	**/
-
 	{
 		// rage::fwBoxStreamerVariable::GetIntersectingAABB
 		auto location = hook::get_pattern<char>("0F 28 0A 48 8B 49 08 4C 8D 25", 10);
 		hook::put<int32_t>(location, (char*)mnbvhList - location + 4);
 		hook::put<int32_t>(location + 31, fwBoxStreamerVariableSize);
 	}
-
-	/**
-	{
-		// rage::fwBoxStreamer::GetIntersectingLine
-		auto address = hook::get_pattern<char>("74 72 48 8B 81 F8 00 00 00", 0xC);
-		hook::put<int32_t>(address, (char*)mnbvhList - address + 4);
-		hook::put<int32_t>(address + 8, fwBoxStreamerVariableSize);
-	}
-	**/
 
 	{
 		// rage::fwBoxStreamerVariable::GetIntersectingLine
